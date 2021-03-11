b21b2ec19750e0c767369e66d33fd3a5f0938d2c
sinbad <sinbadxiii@gmail.com>
2021-03-11 16:58:26 +0500

    config and service provider

---
+++ config/auth.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'auth' => [
+        'defaults' => [
+            'guard' => 'web',
+            'passwords' => 'users',
+        ],
+
+        'guards' => [
+            'web' => [
+                'driver' => 'session',
+                'provider' => 'users',
+            ],
+        ],
+        'providers' => [
+            'users' => [
+                'driver' => 'model',
+                'model'  => \Models\Users::class,
+            ],
+        ],
+        'hash' => [
+            'method' => 'sha1'
+        ],
+    ],
+];
---
+++ src/AuthProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sinbadxiii\PhalconAuth;
+
+use Phalcon\Di\DiInterface;
+use Phalcon\Di\ServiceProviderInterface;
+
+class AuthProvider implements ServiceProviderInterface
+{
+    /**
+     * @var string
+     */
+    protected $providerName = 'auth';
+
+    /**
+     * @param DiInterface $di
+     */
+    public function register(DiInterface $di): void
+    {
+        $di->setShared($this->providerName, function () {
+            return new Auth();
+        });
+    }
+}
