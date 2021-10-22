99ded4d7ec62f6676f0a498753bf22f18ae7cc58
sinbad <sinbadxiii@gmail.com>
2021-10-22 11:17:18 +0500

    fix users.sql

--- db/users.sql
+++ db/users.sql
@@ -7,7 +7,7 @@ CREATE TABLE `users` (
     `username` varchar(50) NOT NULL,
     `name` varchar(255) NOT NULL,
     `email` varchar(100) NOT NULL,
-    `password` varchar(255) NOT NULloginL,
+    `password` varchar(255) NOT NULL,
     `published` tinyint(1) NOT NULL DEFAULT '0',
     `created_at` timestamp NULL,
     `updated_at` timestamp NULL
