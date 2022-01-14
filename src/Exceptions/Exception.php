615ae4ebee6a21fb5b8fba992d0d0c70546ebd78
sinbad <sinbadxiii@gmail.com>
2022-01-14 09:12:57 +0500

    remove phalcon\exception

--- src/Exceptions/Exception.php
+++ src/Exceptions/Exception.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Sinbadxiii\PhalconAuth\Exceptions;
 
-class Exception extends \Phalcon\Exception
+class Exception extends \Exception
 {
     protected $message = 'An error occurred';
 }
