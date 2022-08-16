a71522c2506dc88ae533a538bc4260963cd72ca3
sinbad <sinbadxiii@gmail.com>
2022-08-16 08:53:13 +0500

    readme edit

--- src/RememberingInterface.php
+++ src/RememberingInterface.php
@@ -5,7 +5,6 @@ namespace Sinbadxiii\PhalconAuth;
 interface RememberingInterface
 {
     public function getRememberToken(): ?RememberTokenInterface;
-    public function setRememberToken(RememberTokenInterface $value);
     public function createRememberToken(): RememberTokenInterface;
 
 }
